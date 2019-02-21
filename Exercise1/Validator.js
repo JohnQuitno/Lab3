024d620f553d4879b61ab151dd63db76d913fff1
John Quitno <john.quitno@ku.edu>
2019-02-21 12:48:43 -0600

    added the password.html and validate.js for Exercise1

---
+++ Exercise1/Validator.js
@@ -0,0 +1,11 @@
+function Validate()
+{
+  if(document.getElementById("first").value==document.getElementById("second").value)
+  {
+    alert("Passwords Match");
+  }
+  else
+  {
+    alert("Passwords Don't Match");
+  }
+}
